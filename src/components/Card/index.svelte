0cfaba416d5df0d699462518b5d963faa6b944bc
Luca Galasso <luca.galasso@wonderflow.co>
2020-01-03 14:08:36 +0100

    feat(build): move from rollup to parcel; support sass; improve component structure

---
+++ src/components/Card/index.svelte
@@ -0,0 +1,10 @@
+<script>
+	import classes from './style.scss'
+
+	export let name
+</script>
+
+<div>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+</div>
