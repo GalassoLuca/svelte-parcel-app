36f934a577507e69a933b377212a631ae296c2dd
Luca Galasso <luca.galasso@wonderflow.co>
2020-01-25 18:38:51 +0100

    refactor(components): rename Card into Title

--- src/app.svelte
+++ src/app.svelte
@@ -1,9 +1,9 @@
 <script>
-  import Card from "./components/Card.svelte"
+  import Title from "./components/Title.svelte"
   import Promise from "./components/Promise.svelte"
 
   export let name
 </script>
 
-<Card {name} />
+<Title {name} />
 <Promise />
